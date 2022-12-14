6ccc4086752ea32ffca9627bfe2a0486da2c4f4f
Alex Wagner <alxr.wagner@gmail.com>
2022-12-14 15:20:42 +0700

    Изменил вывод на форматированный. Изменил поиск минимального и максимальных значений в списках.

--- src/Employee.java
+++ src/Employee.java
@@ -1,14 +1,17 @@
 public class Employee {
     private String name;
     private String surname;
-    private String post;
+    private int department;
     private float salary;
+    private int id;
+    private static int counter;
 
-    public Employee(String name, String surname, String post, float salary) {
+    public Employee(String name, String surname, int department, float salary) {
         this.name = name;
         this.surname = surname;
-        this.post = post;
+        this.department = department;
         this.salary = salary;
+        id = ++counter;
     }
 
     public String getName() {
@@ -19,11 +22,23 @@ public class Employee {
         return surname;
     }
 
-    public String getPost() {
-        return post;
+    public int getDepartment() {
+        return department;
     }
 
     public float getSalary() {
         return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
     }
 }
